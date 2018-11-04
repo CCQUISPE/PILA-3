034dc9b7474bd7536fc543f3f96c93be83b277b6
cami63830 <cami63830@gmail.com>
2018-11-04 03:14:46 -0300

    Imprime las vocales

--- PILA 3/Program.cs
+++ PILA 3/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Collections;
 
 namespace PILA_3
 {
@@ -10,6 +11,28 @@ namespace PILA_3
     {
         static void Main(string[] args)
         {
+            
+
+            //Console.Clear();
+            Console.WriteLine("\n El sigeuinte programa imprime las vocales \n");
+            string frase;
+            
+            Stack miPila = new Stack();
+            miPila.Push("A");
+            miPila.Push("E");
+            miPila.Push("I");
+            miPila.Push("O");
+            miPila.Push("U");
+
+            
+            for (byte i = 0; i < 5; i++) // arma el buclne para recorrer la pila
+            {
+                frase = (string)miPila.Pop(); 
+                Console.WriteLine(frase);         // imprime por pantalla
+
+            }
+            Console.ReadKey();
+            
         }
     }
 }
